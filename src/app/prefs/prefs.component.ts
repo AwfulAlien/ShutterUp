4c07d0652d43fffeb5c4aeb4f37bcfd5cccf4cea
AwfulAlien <dhana22busy@gmail.com>
2024-02-13 23:31:12 +0530

    Initial commit + created prefs component + added material design

---
+++ src/app/prefs/prefs.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prefs',
+  standalone: true,
+  imports: [],
+  templateUrl: './prefs.component.html',
+  styleUrl: './prefs.component.css'
+})
+export class PrefsComponent {
+
+}
